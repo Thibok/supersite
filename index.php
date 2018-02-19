e9651044f7235d968ad2dc2c7001804acb13e7ef
Kriyu <thcavailles@laposte.net>
2018-02-19 18:09:37 +0100

    Création de la page d'accueil du site

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Mon super site !
+    </title>
+    
+    <meta charset="utf-8" />
+  </head>
+  
+  <body>
+      <header>
+        <h1><a href="/">Mon super site</a></h1>
+        <p>Comment ça, il n'y a presque rien ?</p>
+      </header>
+      
+      <nav>
+        <ul>
+          <li><a href="/">Accueil</a></li>
+          <li><a href="/admin/">Admin</a></li>
+        </ul>
+      </nav>
+      
+      
+      <section>
+          <p>Article à venir ! </p>
+      </section>
+
+      <footer>
+      </footer>
+  </body>
+</html>
